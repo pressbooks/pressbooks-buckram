6c6494800a56387d6afa1efe7c6cb35475f65bc4
Ned Zimmerman <ned@bight.ca>
2018-04-12 10:38:21 -0300

    Initial commit.

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author  Pressbooks (Book Oven Inc.)
+ * @license GPL 2.0+
+ */
+
+function pressbooks_baseline_theme_setup() {
+	// Add theme support for special features here.
+}
+
+add_action( 'after_setup_theme', 'pressbooks_baseline_theme_setup' );
